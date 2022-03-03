1786509515db788dafbef3e973d78eac1d9da671
Neo <hanxq2@yunjiglobal.com>
2022-03-03 17:02:56 +0800

    update package.json

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: 'entry',
-        corejs: {
-          version: '3.8', // core-js版本号前两位
-          proposals: true,
-        },
-      },
-    ],
+    '@babel/preset-env',
     '@babel/preset-react',
   ],
 }
